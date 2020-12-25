2251819c6ac53aa9aa69fe80410f5e998c48ee00
ccpu <m.doaie@hotmail.com>
2020-12-25 10:17:19 +0330

    added file list

--- src/extension.ts
+++ src/extension.ts
@@ -5,41 +5,41 @@ import { AppModel } from './appModel';
 import { checkNewAnnouncement } from './announcement';
 
 export function activate(context: ExtensionContext) {
-    const appModel = new AppModel();
+  const appModel = new AppModel();
 
-    checkNewAnnouncement(context.globalState);
-    context.subscriptions.push(commands
-        .registerCommand('extension.liveServer.goOnline', async (fileUri) => {
-            await workspace.saveAll();
-            appModel.Golive(fileUri ? fileUri.fsPath : null);
-        })
-    );
+  checkNewAnnouncement(context.globalState);
+  context.subscriptions.push(
+    commands.registerCommand(
+      'extension.liveServer.goOnline',
+      async (fileUri) => {
+        await workspace.saveAll();
+        appModel.Golive(fileUri ? fileUri.fsPath : null);
+      }
+    )
+  );
 
-    context.subscriptions.push(commands
-        .registerCommand('extension.liveServer.goOffline', () => {
-            appModel.GoOffline();
-        })
-    );
+  context.subscriptions.push(
+    commands.registerCommand('extension.liveServer.goOffline', () => {
+      appModel.GoOffline();
+    })
+  );
 
-    context.subscriptions.push(commands
-        .registerCommand('extension.liveServer.changeWorkspace', () => {
-            appModel.changeWorkspaceRoot();
-        })
-    );
+  context.subscriptions.push(
+    commands.registerCommand('extension.liveServer.changeWorkspace', () => {
+      appModel.changeWorkspaceRoot();
+    })
+  );
 
-    // context.subscriptions.push(window
-    //     .onDidChangeActiveTextEditor(() => {
-    //         if (window.activeTextEditor === undefined) return;
-    //         if (workspace.rootPath === undefined && Helper.IsSupportedFile(window.activeTextEditor.document.fileName)) {
-    //             StatusbarUi.Init();
-    //         }
-    //     })
-    // );
+  // context.subscriptions.push(window
+  //     .onDidChangeActiveTextEditor(() => {
+  //         if (window.activeTextEditor === undefined) return;
+  //         if (workspace.rootPath === undefined && Helper.IsSupportedFile(window.activeTextEditor.document.fileName)) {
+  //             StatusbarUi.Init();
+  //         }
+  //     })
+  // );
 
-    context.subscriptions.push(appModel);
+  context.subscriptions.push(appModel);
 }
 
-
-export function deactivate() {
-
-}
+export function deactivate() {}
