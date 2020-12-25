60a2bcff7b74f1aa4b470ff5e85f2e595a1fe342
ccpu <m.doaie@hotmail.com>
2020-12-25 10:56:18 +0330

    bump packages

--- src/LiveServerHelper.ts
+++ src/LiveServerHelper.ts
@@ -1,40 +1,38 @@
 'use strict';
 import * as liveServer from 'live-server';
+// @ts-ignore
 import * as httpShutdown from 'http-shutdown';
 
 export class LiveServerHelper {
-
-    static StartServer(params, callback) {
+  static StartServer(params, callback) {
+    setTimeout(() => {
+      try {
+        let ServerInstance = liveServer.start(params);
         setTimeout(() => {
-            try {
-                let ServerInstance = liveServer.start(params);
-                setTimeout(() => {
+          if (!ServerInstance._connectionKey) {
+            return callback({});
+          }
 
-                    if (!ServerInstance._connectionKey) {
-                        return callback({});
-                    }
-
-                    httpShutdown(ServerInstance);
-                    return callback(ServerInstance);
-
-                }, 1000);
-            } catch (err) {
-                console.error(err);
-                callback({
-                    errorMsg: err
-                });
-            }
-
-        }, 0);
-
-    }
-
-    static StopServer(LiveServerInstance, callback) {
-        LiveServerInstance.shutdown(() => {
-            // callback(); /*only Working first time, Unknown Bug*/
+          httpShutdown(ServerInstance);
+          return callback(ServerInstance);
+        }, 1000);
+      } catch (err) {
+        console.error(err);
+        callback({
+          errorMsg: err,
         });
-        LiveServerInstance.close();
-        liveServer.shutdown();
-        setTimeout(() => { callback(); }, 1000);
-    }
+      }
+    }, 0);
+  }
+
+  static StopServer(LiveServerInstance, callback) {
+    LiveServerInstance.shutdown(() => {
+      // callback(); /*only Working first time, Unknown Bug*/
+    });
+    LiveServerInstance.close();
+    liveServer.shutdown();
+    setTimeout(() => {
+      callback();
+    }, 1000);
+  }
 }
